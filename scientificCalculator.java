380cf4b1a619a86a82aa3cb36d718915f9a2a5f7
albaraa-prog <albraa9021@gmail.com>
2025-07-20 14:41:34 +0400

    inin

---
+++ scientificCalculator.java
@@ -0,0 +1,81 @@
+public class scientificCalculator {
+
+        public static double Multiply(double a, double b) {
+            return a * b;
+        }
+
+        public static double Add(double a, double b){
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b){
+            return a - b;
+        }
+
+        public static double Division(double a, double b){
+            return a / b;
+        }
+
+        public static double squareRoot(double a){
+            return Math.sqrt(a);
+        }
+
+        public static double Power(double a, double b){
+            return Math.pow(a ,b);
+        }
+
+        public static double Sine(double a){
+            return Math.sin(a);
+        }
+
+        public static double Cosine(double a){
+            return Math.cos(a);
+        }
+
+        public static double Tangent(double a){
+            return Math.tan(a);
+        }
+
+        public static double naturalLogarithm(double a){
+            return Math.log(a);
+        }
+
+        public static double logarithmBase10(double a){
+            return Math.log10(a);
+        }
+
+        public static double asoluteValue(double a){
+            return Math.abs(a);
+        }
+
+        public static double Round(double a){
+            return Math.round(a);
+        }
+
+        public static double Ceiling(double a){
+            return Math.ceil(a);
+        }
+
+        public static double Floor(double a){
+            return Math.floor(a);
+        }
+
+        public static double Minimum(double a, double b){
+            return Math.min(a, b);
+        }
+
+        public static double Maximum(double a, double b){
+            return Math.max(a, b);
+        }
+
+        public static void displayMenu(){
+
+        }
+
+        class Main {
+            public static void main(String[] args) {
+
+
+            }
+        }
+}
